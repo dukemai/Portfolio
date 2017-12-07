c9501f0473902e3172525ece1ce8735ba6ef5ae5
duke mai <duc.mai@niteco.se>
2017-12-07 23:42:58 +0100

    update static files

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -1,6 +1,20 @@
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'custom-sass-loader',
+      options: {
+        postCssPlugins: [
+          autoprefixer({
+            browsers: ['last 2 versions'],
+          })
+        ]
+      }
+    }
+  ],
 }
--- www/src/layouts/index.js
+++ www/src/layouts/index.js
@@ -1,58 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import 'bulma/css/bulma.css';
 
-import './index.css';
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-);
+import Header from './Header';
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Portfolio"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+    <div className="container is-fluid" >
+      <Header />
+      <div>
+        {children()}
+      </div>
     </div>
   </div>
 );
